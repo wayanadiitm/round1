902d47bc0724d118f6c067e7163ed62c05877141
wayanad house 2023 archives <150238196+wayanadiitm@users.noreply.github.com>
2025-03-09 23:33:14 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wayanadiitm.github.io',
-  base: 'round1',
+  site: 'https://wayanadiitm.github.io/round1/',
+  //base: 'round1',
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
